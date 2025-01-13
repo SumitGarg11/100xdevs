1a4f5dd1e4505d92caa4e8406f5fca71a970f528
SumitGarg <garg10sumit@gmail.com>
2025-01-13 10:05:55 +0530

    commit

--- week07_1/src/store/atom/count.jsx
+++ week07_1/src/store/atom/count.jsx
@@ -2,5 +2,5 @@ import { atom } from "recoil";
 
 export const countAtom = atom({
   key: "countAtom", // unique ID (with respect to other atoms)
-  default: 0, // default value
+  default: 0, // sumit
 });
