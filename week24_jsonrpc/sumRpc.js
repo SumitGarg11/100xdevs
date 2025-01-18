97c2360fa11edf044c8dbacf26ea1a7dfc067a02
SumitGarg <garg10sumit@gmail.com>
2025-01-18 22:09:18 +0530

    commit

---
+++ week24_jsonrpc/sumRpc.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+app.use(express.json());
+function addNum(a, b) {
+  return a + b;
+}
+app.post("/rpc", function (req, res) {
+  const { a, b } = req.body;
+  if (typeof a !== "number" || typeof b !== "number") {
+    res
+      .status(400)
+      .json({ error: "Invalid input. Both a and b should be numbers." });
+    return;
+  }
+  const result = addNum(a, b);
+  res.json({ result });
+});
+app.listen(3000, () => {
+  console.log("listening on port 3000");
+});
